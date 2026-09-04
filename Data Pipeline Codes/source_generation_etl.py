ccaef93ad3f0bd47ba4e79a26fd77a7c9c305be3
Tanmay Melanta <tanmaymelanta@gmail.com>
2026-09-04 19:59:54 +0530

    Update source_generation_etl.py

--- Data Pipeline Codes/source_generation_etl.py
+++ Data Pipeline Codes/source_generation_etl.py
@@ -56,7 +56,6 @@ def source_generation_etl():
     ]
 
     changed_files = os.environ.get("CHANGED_FILES", "").strip()
-    print(os.environ.get("CHANGED_FILES"))
     if changed_files:
         parquet_files = [f.strip() for f in changed_files.splitlines() if f.strip().endswith(".parquet")]
         parquet_files = [f for f in parquet_files if os.path.isfile(f)]
